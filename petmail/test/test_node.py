674896c04eedc3dfa2e4ca1c698adf64782b6b4b
Brian Warner <warner@lothar.com>
2013-07-28 17:36:23 -0700

    add node-as-child-Service framework for CLi tests

--- petmail/test/test_node.py
+++ petmail/test/test_node.py
@@ -4,7 +4,9 @@ from twisted.trial import unittest
 from twisted.internet import threads # CLI tests use deferToThread
 from twisted.internet import defer
 from twisted.internet.utils import getProcessOutputAndValue
-from ..scripts import runner
+from twisted.application import service
+from ..scripts import runner, startstop
+from ..scripts.create_node import create_node
 
 class Basic(unittest.TestCase):
     def test_one(self):
@@ -78,7 +80,31 @@ class Run(CLIinProcessMixin, BasedirMixin, unittest.TestCase):
         d.addBoth(self.anyways, self.cliMustSucceed, "stop", basedir)
         return d
 
-class CLI(CLIinThreadMixin, BasedirMixin, unittest.TestCase):
+class NodeRunnerMixin:
+    def setUp(self):
+        self.sparent = service.MultiService()
+        self.sparent.startService()
+
+    def tearDown(self):
+        return self.sparent.stopService()
+
+    def createNode(self, basedir):
+        so = runner.CreateNodeOptions()
+        so.parseOptions([basedir])
+        out,err = StringIO(), StringIO()
+        rc = create_node(so, out, err)
+        self.failUnlessEqual(rc, 0, (rc, out, err))
+        return rc, out ,err
+
+    def startNode(self, basedir):
+        so = runner.StartNodeOptions()
+        so.parseOptions([basedir])
+        p = startstop.MyPlugin(basedir, os.path.join(basedir, "petmail.db"))
+        n = p.makeService(so)
+        n.setServiceParent(self.sparent)
+        return n
+
+class CLI(CLIinThreadMixin, BasedirMixin, NodeRunnerMixin, unittest.TestCase):
     def test_create(self):
         basedir = os.path.join(self.make_basedir(), "node1")
         d = self.cliMustSucceed("create-node", basedir)
@@ -87,3 +113,8 @@ class CLI(CLIinThreadMixin, BasedirMixin, unittest.TestCase):
             self.failUnless(os.path.exists(os.path.join(basedir, "petmail.db")))
         d.addCallback(_check)
         return d
+
+    def test_api(self):
+        basedir = os.path.join(self.make_basedir(), "node1")
+        self.createNode(basedir)
+        n = self.startNode(basedir)
