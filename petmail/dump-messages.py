ad18e23654ba8d07b7aa3f31e64a49cc781a4dc6
Brian Warner <warner@lothar.com>
2013-08-11 03:22:35 -0700

    add a debugging tool

---
+++ petmail/dump-messages.py
@@ -0,0 +1,10 @@
+
+import os, sys
+from nacl.signing import VerifyKey
+
+for fn in sys.argv[1:]:
+    vk = VerifyKey(fn.split("/")[-2].decode("hex"))
+    sm = open(fn,"rb").read()
+    assert sm.startswith("r0:")
+    m = vk.verify(sm[len("r0:"):].decode("hex"))
+    print fn.split("/")[-1][:10], ":", repr(m[:12]), "..."
