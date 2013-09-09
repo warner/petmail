03d0a0b1fca269cc8f2363a693a067a4d751ae86
Brian Warner <warner@lothar.com>
2013-09-08 21:54:50 -0700

    web_client: use separate subscription objects per channel

--- petmail/rendezvous/web_client.py
+++ petmail/rendezvous/web_client.py
@@ -1,6 +1,8 @@
 from twisted.python import log, failure
+from twisted.internet import defer
 from twisted.application import service, internet
 from twisted.web import client
+from ..eventual import eventually
 from ..invitation import VALID_INVITEID, VALID_MESSAGE
 from .. import eventsource
 
@@ -8,8 +10,89 @@ from .. import eventsource
 # required to do so. It is not required to check version numbers ("r0:") or
 # signatures.
 
-class Marker:
-    pass
+class ChannelWatcher(service.MultiService):
+    """I handle a single channel. I subscribe immediately. I live until I am
+    unsibscribed and the last pending request or send is retired"""
+    def __init__(self, rclient, channelID, url, enable_polling, polling_interval):
+        service.MultiService.__init__(self)
+        self.rclient = rclient
+        self.channelID = channelID
+        self.url = url
+        self.pending_request = False
+        self.pending_send = False
+        self.outbound_queue = []
+        self.ts = None
+        if enable_polling:
+            self.ts = internet.TimerService(polling_interval, self.poll)
+            self.ts.setServiceParent(self)
+
+    def maybeDisown(self):
+        if self.pending_request or self.pending_send:
+            return
+        assert not self.outbound_queue
+        self.disownServiceParent()
+
+    def unsubscribe(self):
+        if self.ts:
+            self.ts.disownServiceParent() # stop any polling
+            del self.ts
+        self.maybeDisown()
+
+    def poll(self):
+        if self.pending_request:
+            return # we're busy, call again later
+        self.pending_request = True
+
+        # create a watcher for this channel
+        def _handler(name, data):
+            if name == "data":
+                # we expect the value to start with "r0:" followed by a
+                # hex-encoded signed message
+                self.rclient.messagesReceived(self.channelID, set([data]))
+        d = eventsource.get_events(self.url, _handler)
+        # If the relay can do EventSource, the handler will be fed with
+        # incoming events, and the Deferred won't fire until they hang up. If
+        # it can't, get_events() will do a regular GET, and the handler will
+        # be fed, and the Deferred will fire. If an error occurs, it will
+        # fire with a Failure.
+
+        # so if we're still subscribed when the Deferred fires, we should
+        # reschedule for later.
+        d.addBoth(self._http_done)
+
+    def _http_done(self, res):
+        self.pending_request = False
+        if isinstance(res, failure.Failure):
+            log.msg("HTTP error polling %s: %s" % (self.channelID, res))
+        # if we're still subscribed, the poller will trigger again later. no
+        # need to do anything now.
+        self.maybeDisown()
+
+    def send(self, msg):
+        d = defer.Deferred()
+        self.outbound_queue.append((msg,d))
+        self.maybeSend()
+        return d # fires when this message is retired
+
+    def maybeSend(self):
+        if not self.outbound_queue:
+            return
+        if self.pending_send:
+            return
+        self.pending_send = True
+        msg,done_deferred = self.outbound_queue.pop(0)
+        d = client.getPage(self.url, method="POST", postdata=msg)
+        def _sent(resp):
+            log.msg("HTTP POST to %s happy: %s" % (self.channelID, resp))
+            eventually(done_deferred.callback, resp)
+        def _err(f):
+            log.err(f)
+            eventually(done_deferred.callback, f)
+        d.addCallbacks(_sent, _err)
+        def _done(_):
+            self.pending_send = False
+            self.maybeSend()
+        d.addBoth(_done)
 
 class HTTPRendezvousClient(service.MultiService):
     """I talk to a remote HTTP-based rendezvous server."""
@@ -21,83 +104,37 @@ class HTTPRendezvousClient(service.MultiService):
         service.MultiService.__init__(self)
         self.baseurl = baseurl
         assert self.baseurl.endswith("/")
-        self.subscriptions = set()
-        self.pending_requests = set()
-        self.pending_sends = set()
+        self.subscriptions = {}
 
     def is_idle(self):
-        if self.pending_requests:
-            return False
-        if self.pending_sends:
-            return False
-        return True
+        return not list(self)
 
     def subscribe(self, channelID):
         assert VALID_INVITEID.search(channelID), channelID
-        self.subscriptions.add(channelID)
-        if len(self.subscriptions) == 1 and self.enable_polling:
-            self.ts = internet.TimerService(self.polling_interval, self.poll)
-            self.ts.setServiceParent(self)
+        assert channelID not in self.subscriptions
+        c = ChannelWatcher(self, channelID,
+                           self.baseurl + "relay/" + channelID,
+                           self.enable_polling, self.polling_interval)
+        self.subscriptions[channelID] = c
+        c.setServiceParent(self)
+
+    def messagesReceived(self, channelID, messages):
+        if channelID not in self.subscriptions:
+            return # too late
+        self.parent.messagesReceived(channelID, messages)
 
     def unsubscribe(self, channelID):
-        self.subscriptions.remove(channelID)
-        if not self.subscriptions and self.enable_polling:
-            self.ts.disownServiceParent()
-            del self.ts
+        self.subscriptions[channelID].unsubscribe()
+        # they will disconnect themselves when ready
 
     def poll(self):
         #print "entering poll"
         # we may unsubscribe while in the loop, so copy self.subscriptions
-        for channelID in list(self.subscriptions):
-            self.pollChannel(channelID)
-
-    def pollChannel(self, channelID):
-        if channelID in self.pending_requests:
-            return
-        url = self.baseurl + "relay/" + channelID
-        self.pending_requests.add(channelID)
-
-        # create a watcher for this channel
-        def _handler(name, data):
-            if name == "data":
-                # we expect the value to start with "r0:" followed by a
-                # hex-encoded signed message
-                self.parent.messagesReceived(channelID, set([data]))
-        d = eventsource.get_events(url, _handler)
-        # If the relay can do EventSource, the handler will be fed with
-        # incoming events, and the Deferred won't fire until they hang up. If
-        # it can't, get_events() will do a regular GET, and the handler will
-        # be fed, and the Deferred will fire. If an error occurs, it will
-        # fire with a Failure.
-
-        # so if we're still subscribed when the Deferred fires, we should
-        # reschedule for later.
-        d.addBoth(self._http_done, channelID)
-
-    def _http_done(self, res, channelID):
-        self.pending_requests.remove(channelID)
-        if isinstance(res, failure.Failure):
-            log.msg("HTTP error polling %s: %s" % (channelID, res))
-        # if we're still subscribed, the poller will trigger again later. no
-        # need to do anything now.
+        for c in self.subscriptions.values()[:]:
+            c.poll()
 
     def send(self, channelID, msg):
         assert isinstance(channelID, str)
-        assert channelID in self.subscriptions
         assert VALID_INVITEID.search(channelID), channelID
         assert VALID_MESSAGE.search(msg), msg
-
-        url = self.baseurl + "relay/" + channelID
-        m = Marker()
-        self.pending_sends.add(m)
-        d = client.getPage(url, method="POST", postdata=msg)
-        def _done(res):
-            self.pending_sends.remove(m)
-            return res
-        d.addBoth(_done)
-        def _sent(resp):
-            log.msg("HTTP POST to %s happy: %s" % (channelID, resp))
-        def _err(f):
-            log.err(f)
-        d.addCallbacks(_sent, _err)
-        return d
+        return self.subscriptions[channelID].send(msg)
