499dc24c32845bb02cb307192b52d49252457ee6
Brian Warner <warner@lothar.com>
2014-08-24 23:53:36 -0700

    test_system: exercise offer-mailbox/accept-mailbox

--- petmail/test/test_system.py
+++ petmail/test/test_system.py
@@ -1,4 +1,4 @@
-import os
+import os, re, json
 from twisted.trial import unittest
 from .common import BasedirMixin, NodeRunnerMixin, CLIinThreadMixin
 from .pollmixin import PollMixin
@@ -52,4 +52,43 @@ class System(CLIinThreadMixin, BasedirMixin, NodeRunnerMixin, PollMixin,
             self.failUnlessEqual(m["payload"], {"basic": "hello bob"})
         d.addCallback(_check_message)
 
+        return d
+
+    def test_mailbox(self):
+        # two nodes, using a localdir relay, and --local-mailbox
+        self.basedir1 = os.path.join(self.make_basedir(), "node1")
+        self.createNode(self.basedir1)
+        n1 = self.startNode(self.basedir1)
+        self.basedir2 = os.path.join(self.make_basedir(), "node2")
+        self.createNode(self.basedir2)
+        n2 = self.startNode(self.basedir2)
+        n2_mailboxes = n2.db.execute("SELECT * FROM mailboxes").fetchall()
+        self.failUnlessEqual(len(n2_mailboxes), 0)
+
+        d = self.cli_1("offer-mailbox", "bob")
+        def _offered(res):
+            mo = re.search(r'code=([\w\-]+)', res)
+            code = mo.group(1)
+            return code
+        d.addCallback(_offered)
+        d.addCallback(lambda code:
+                      self.cli_2("accept-mailbox", "-n", "alice", code))
+
+        def until_invited():
+            a1 = n1.agent.command_list_addressbook()
+            if not a1 or not a1[0]["acked"]:
+                return False
+            self.cid_bob = a1[0]["cid"]
+            a2 = n2.agent.command_list_addressbook()
+            if not a2 or not a2[0]["acked"]:
+                return False
+            return True
+        d.addCallback(lambda _: self.poll(until_invited))
+
+        def _accepted(res):
+            n2_mailboxes = n2.db.execute("SELECT * FROM mailboxes").fetchall()
+            self.failUnlessEqual(len(n2_mailboxes), 1)
+            mr = json.loads(n2_mailboxes[0]["mailbox_record_json"])
+            self.failUnlessEqual(mr["transport"]["generic"]["type"], "http")
+        d.addCallback(_accepted)
         return d
